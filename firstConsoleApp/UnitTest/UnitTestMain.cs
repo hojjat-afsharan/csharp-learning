e52c06137cc24433c2ae2dd959e94cebae0435dd
somic723 <afsharan.h@gmail.com>
2018-12-27 15:39:26 +0330

    Add Unit tests

---
+++ firstConsoleApp/UnitTest/UnitTestMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTest {
+    [TestFixture]
+    public class UnitTestMain {
+        [Test]
+        public void ShowAvailableTickets_ifOnSaleIsNull_ReturnZero() {
+            LearnCsharp.ticketsOnSale = null;
+            int availableTickets = LearnCsharp.CalculateTickets();
+            Assert.AreEqual(availableTickets, 0);
+        }
+        
+        [Test]
+        public void ShowAvailableTickets_ifOnSaleIsInt_ReturnSameInt() {
+            LearnCsharp.ticketsOnSale = 10;
+            int availableTickets = LearnCsharp.CalculateTickets();
+            Assert.AreEqual(availableTickets, 10);
+        }
+
+        [Test]
+        public void ShowAvailableTickets_ifOnSaleIsBelowZero_ReturnZero() {
+            LearnCsharp.ticketsOnSale = -10;
+            int availableTickets = LearnCsharp.CalculateTickets();
+            Assert.AreEqual(availableTickets, 0);
+        }
+
+        [Test]
+        public void NullCheck_ifIsNotNull_ReturnSomeInt() {
+            LearnCsharp.ticketsOnSale = 10;
+            int availableTickets = LearnCsharp.NullChecks();
+            Assert.AreEqual(availableTickets, 10);
+        }
+    }
+}
--- firstConsoleApp/firstConsoleApp/Program.cs
+++ firstConsoleApp/firstConsoleApp/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace firstConsoleApp
+public class LearnCsharp
 {
-    class Program
+
+    public static int? ticketsOnSale = null;
+
+    public static int Main()
     {
-        static void Main(string[] args)
-        {
+        return CalculateTickets();
+    }
+
+    public static int CalculateTickets() {
+        int availableTickets;//= ticketsOnSale ?? 0;
+
+        if(ticketsOnSale == null || ticketsOnSale < 0) {
+            availableTickets = 0;
+        } else {
+            availableTickets = ticketsOnSale.Value;// or (int)ticketsOnSale
         }
+
+        return availableTickets;
+    }
+
+    public static int NullChecks() {
+        int availableTickets = ticketsOnSale ?? 0;
+
+        return availableTickets;
     }
 }
+
